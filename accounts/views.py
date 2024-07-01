faec7dbdae8c3401cc6fc8801d396726f35c3276
s0obang <tnqls40168@gmail.com>
2024-07-01 15:56:08 +0900

    feat:로그인, 로그아웃

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib import auth 
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.models import User 
+from django.shortcuts import render, redirect 
+from django.contrib.auth import authenticate
+
+
+
+def signup(request): 
+    if request.method == 'POST': 
+        if request.POST['password1'] == request.POST['password2']: 
+            user = User.objects.create_user( 
+                username=request.POST['username'], 
+                password=request.POST['password1'], 
+                email=request.POST['email'],
+            ) 
+            auth.login(request, user) 
+            return redirect('/') 
+        return render(request, 'signup.html') 
+    else: 
+        form = UserCreationForm 
+        return render(request, 'signup.html', {'form':form})
+    
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def home(request):
+    return render(request, 'home.html')
