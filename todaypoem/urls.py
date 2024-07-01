918a3b486a47961e2ee12d937263bf8944b4dd9d
s0obang <tnqls40168@gmail.com>
2024-07-01 10:01:01 +0900

    urls

--- todaypoem/urls.py
+++ todaypoem/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('word/', include('poem.urls')),
+    path('poem/', include('poem.urls')),
 ]
