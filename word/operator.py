7b7fd39b44a5d0ed19fea6bab824edccfff91008
minnoiii <shmin1202@gmail.com>
2024-07-05 18:51:29 +0900

    seohyun change2

--- word/operator.py
+++ word/operator.py
@@ -8,7 +8,7 @@ def start():
     scheduler=BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
     register_events(scheduler)
-    @scheduler.scheduled_job('cron', hour=21, minute=22, second=33, name = 'wordPost')
+    @scheduler.scheduled_job('cron', hour=17, minute=29, second=3, name = 'wordPost')
     def auto_wordPost():
         wordPost()
     scheduler.start()
